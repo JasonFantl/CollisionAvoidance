9ec34cb800c1877544f0231211148ec96b174a97
JasonFantl <jasonemerald@gmail.com>
2024-08-03 20:10:56 -0400

    fixed bug and generalized RVO

--- velocityObstacle.js
+++ velocityObstacle.js
@@ -8,7 +8,7 @@ function collisionConeAngle(position, radius) {
 // relative to the position of boid_A
 class VelocityObstacle {
   constructor(boid_A, boid_B) {
-    let safety_margin_factor = 1.05;
+    let safety_margin_factor = 1.1;
     let relativePosition = p5.Vector.sub(boid_B.position, boid_A.position);
     let angleToPosition = atan2(relativePosition.y, relativePosition.x);
     let coneAngle = collisionConeAngle(
@@ -17,18 +17,21 @@ class VelocityObstacle {
     );
 
     // NOTE: Swap from VO to RVO
-    // this.origin = boid_B.velocity.copy(); // VO 
-    this.origin = p5.Vector.add(boid_A.velocity, boid_B.velocity).mult(0.5); // RVO
+    // // VO 
+    // this.origin = boid_B.velocity.copy(); 
+    // RVO
+    let alpha = 0.5;
+    this.origin = p5.Vector.add(p5.Vector.mult(boid_A.velocity, alpha), p5.Vector.mult(boid_B.velocity, 1 - alpha));
 
     this.left_ray = p5.Vector.fromAngle(angleToPosition + coneAngle / 2);
     this.right_ray = p5.Vector.fromAngle(angleToPosition - coneAngle / 2);
   }
 
+  // TODO: Optimize this calculation. WARNING! This needs to be carefully implemented, covering edge cases like >= 180 degree rays.
   contains(position) {
-    let A = this.left_ray;
-    let B = this.right_ray;
-    let C = p5.Vector.sub(position, this.origin).normalize();
+    let angle_to_right_ray = (this.left_ray.angleBetween(this.right_ray) + TWO_PI) % TWO_PI;
+    let angle_to_in_ray = (this.left_ray.angleBetween(p5.Vector.sub(position, this.origin)) + TWO_PI) % TWO_PI;
 
-    return A.dot(C) >= A.dot(B) && B.dot(C) >= A.dot(B);
+    return angle_to_right_ray < angle_to_in_ray;
   }
 }
